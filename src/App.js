1ce87fffb3d35056356ec955f2e8e6a0f0896ef7
IddrisuAminu <iddrisuaminuict@gmail.com>
2022-09-29 10:02:31 +0000

    first commit on kohisong fronend

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+// import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./screens/Home";
+import { Container } from "react-bootstrap";
+import RegisterUserScreen from "./screens/RegisterUserScreen";
+import Items from "./screens/Items";
+import AddItem from "./screens/AddItem";
+import LoginScreen from "./screens/LoginScreen";
+import Footer from "./components/Footer";
+import ProductListPage from "./screens/ProductListPage";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/items" element={<Items/>} />
+          <Route path="/additem" element={<AddItem/>} />
+          <Route path="/login" element={<LoginScreen/>} />
+          <Route path="/register" element={<RegisterUserScreen/>} />
+          <Route path="/products" element={<ProductListPage/>} />
+        </Routes>
+      </Container>
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
